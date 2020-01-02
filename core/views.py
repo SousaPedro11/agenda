3e731f10385a0ed4497d638d2048cf4dc7a1f697
Pedro Sousa <ppls2106@gmail.com>
2020-01-02 15:46:37 -0300

    manipulate views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+
 
 # Create your views here.
+def lista_eventos(request):
+    usuario = request.user
+    eventos = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos': eventos}
+    return render(request, 'agenda.html', dados)
+
+
+# def index(request):
+#     return redirect('/agenda')
